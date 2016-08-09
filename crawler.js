0e1ed80e3f0bd51ee35402d229fa5b8f96187acb
Edward Lu <edward.lu@centricconsulting.com>
2016-08-09 11:22:49 -0500

    optimize

--- crawler.js
+++ crawler.js
@@ -1,17 +1,25 @@
-setInterval(function(){}, 1000); // 5000 milliseconds
-
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
-var person = "Noctierre";
 
-var START_URL = "http://na.op.gg/summoner/userName=" + person;
+//User defined variables
+var person = "Noctierre";
+var server = "na";
+
+var START_URL = ("http://" + server + ".op.gg/summoner/userName=" + person);
 var url = new URL(START_URL);
 
-var SEARCH_WORD = "Platinum";
+//Potential divisions
+var SEARCH_CHALLENGER = "Challenger";
+var SEARCH_MASTER = "Master";
+var SEARCH_DIA = "Diamond";
+var SEARCH_PLAT = "Platinum";
+var SEARCH_GOLD = "Gold";
+var SEARCH_SILVER = "Silver";
+var SEARCH_BRONZE = "Bronze";
 
-var pageToVisit = "http://na.op.gg/summoner/userName=" + person;
-console.log("Visiting page " + pageToVisit);
+var pageToVisit = "http://" + server + ".op.gg/summoner/userName=" + person;
+console.log("Visiting " + pageToVisit);
 
 request(pageToVisit, function(error, response, body) {
     if(error) {
@@ -21,29 +29,54 @@ request(pageToVisit, function(error, response, body) {
     if(response.statusCode === 200) {
         // Parse the document body
         var $ = cheerio.load(body);
-        console.log("Running a checker on op.gg to check " + person + "'s Rank..");
+        var name = cheerio.load('<head>Hello</head>')
+
+        console.log("Pulling data from op.gg to check " + "'" + person + "'s current rank..");
 
         // true or false
-        var isWordFound = searchForWord($, SEARCH_WORD);
+        var isWordFound = searchForWord($, SEARCH_PLAT);
+        if (!isWordFound) {
+            isWordFound = searchForWord($, SEARCH_DIA);
+        }
+        if (!isWordFound) {
+            isWordFound = searchForWord($, SEARCH_GOLD);
+        }
+        if (!isWordFound) {
+            isWordFound = searchForWord($, SEARCH_MASTER);
+        }
+        if (!isWordFound) {
+            isWordFound = searchForWord($, SEARCH_CHALLENGER);
+        }
         var hisRanking = returnRanking($, 'LP');
 
         if(isWordFound) {
-            console.log(person + " is currently: " + hisRanking);
+            console.log(person + " is currently: " + '\n' + hisRanking);
+            if (hisRanking.charAt(0) === 'P')
+            {
+                console.log(person + " is still Platinum, unlucky :|");
+            }
+
             if (hisRanking.charAt(0) === 'G')
             {
                 console.log('Definitely find a new game');
-            }
-            if (hisRanking.charAt(0) === 'P')
-            {
-                console.log(person + " is still Platinum, unlucky");
             }
 
             if (hisRanking.charAt(0) === 'D')
             {
                 console.log(person + " is not bad now! Congrats!");
             }
+
+            if (hisRanking.charAt(0) === 'M')
+            {
+                console.log("You're pretty talented!");
+            }
+
+            if (hisRanking.charAt(0) === 'C')
+            {
+                console.log("Why are using this");
+            }
         } else {
-            console.log("Couldn't find " + person + " :(");
+            console.log("Couldn't find " + person + " :|");
         }
     }
 });
@@ -57,38 +90,58 @@ function returnRanking($, word) {
     var bodyText = $('html > body').text();
     var temp = bodyText.indexOf(word);
 
-    var x = bodyText.indexOf('Platinum');
+    //TODO: FIX MODIFY 'SEARCH_PLAT' TO 'SEARCH_X'
+    var x = bodyText.indexOf(SEARCH_PLAT);
+
+    var rank = "";
 
     //Get rank and division
-    var rank_1 = bodyText.charAt(x);
-    var rank_2 = bodyText.charAt(x+1);
-    var rank_3 = bodyText.charAt(x+2);
-    var rank_4 = bodyText.charAt(x+3);
-    var rank_5 = bodyText.charAt(x+4);
-    var rank_6 = bodyText.charAt(x+5);
-    var rank_7 = bodyText.charAt(x+6);
-    var rank_8 = bodyText.charAt(x+7);
-    var rank_9 = bodyText.charAt(x+8);
-    var rank_10 = bodyText.charAt(x+9);
-    var rank = rank_1+rank_2+rank_3+rank_4+rank_5+rank_6+rank_7+rank_8+rank_9+rank_10;
+    if (SEARCH_CHALLENGER || SEARCH_PLAT) {
+        for (var i = x; i < x+10; i++) {
+            rank = rank + bodyText.charAt(i);
+            //console.log("your rank is: " + rank);
+        }
+    }
+
+    else if (SEARCH_MASTER || SEARCH_SILVER || SEARCH_BRONZE) {
+        for (var i = x; i < x+6; i++) {
+            rank = rank + bodyText.charAt(i);
+        }
+    }
+
+    else if (SEARCH_DIA) {
+        for (var i = x; i < x+7; i++) {
+            rank = rank + bodyText.charAt(i);
+        }
+    }
+
+    else if (SEARCH_GOLD) {
+        for (var i = x; i < x+4; i++) {
+            rank = rank + bodyText.charAt(i);
+        }
+    }
 
     //Get points and lp
+    var lp_7 = bodyText.charAt(temp-4);
     var lp_8 = bodyText.charAt(temp-3);
     var lp_9 = bodyText.charAt(temp-2);
-    var lp_10 = bodyText.charAt(temp-1);
-    var lp_11 = bodyText.charAt(temp);
-    var lp_12 = bodyText.charAt(temp+1);
-    var lp = lp_8+lp_9+lp_10+lp_11+lp_12;
+    var temporary = bodyText.substring(temp-1, temp+2);
+
+    //handle 100lp case
+    if (((lp_8 === '0') && (lp_9 === '0'))){
+        var lp = lp_7 + lp_8 + lp_9 + temporary;
+    }
+
+    //handle 0lp case
+    else if (lp_9 === 10) {
+        var lp = lp_9 + temporary;
+    }
+
+    //handle XX lp case
+    else {
+        var lp = lp_8 + lp_9 + temporary;
+    }
 
     //Calculate result
-    var good_or_bad = rank + " " + lp;
-
-    return good_or_bad;
+    return rank + '\n' + lp;
 }
-
-
-
-
-
-
-
